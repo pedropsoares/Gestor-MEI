6f9682878fab528a0d6bed9e5327dd8777dc004b
deyvid gonçalves <deyvidg.lins@gmail.com>
2019-10-24 23:45:06 -0300

    raiz do projeto

--- app/src/main/java/com/example/gestormei/ClienteDados.java
+++ app/src/main/java/com/example/gestormei/ClienteDados.java
@@ -6,78 +6,113 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 import java.util.ArrayList;
 
-public class ClienteDados extends SQLiteOpenHelper {
+/**
+ * Created by Melo on 19/12/2017.
+ */
 
-    public static final String DATABASE_NAME = "GestorMEI.db";
+public class AlunoDados extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "TeAmoProfessorMelo.db";
     private static final int DATABASE_VERSION = 2;
 
-    public static final String CLIENTE_TABELA_NOME = "cliente";
-    public static final String CLIENTE_COLUNA_NOME= "nome";
-    public static final String CLIENTE_COLUNA_EMAIL = "email";
-    public static final String CLIENTE_COLUNA_CELULAR = "celular";
-    public static final String CLIENTE_COLUNA_CEP = "cep";
-    public static final String CLIENTE_COLUNA_NUMERO = "numero";
+    public static final String ALUNO_TABELA_NOME = "aluno";
+    public static final String ALUNO_COLUNA_MATRICULA = "matricula";
+    public static final String ALUNO_COLUNA_NOME = "nome";
+    public static final String ALUNO_COLUNA_CPF = "cpf";
+    public static final String ALUNO_COLUNA_RELEVANCIA = "relevancia";
+
+    public AlunoDados(Context context) {
+        super(context, DATABASE_NAME , null, DATABASE_VERSION);
 
 
-    public ClienteDados(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + CLIENTE_TABELA_NOME +
-                "(" + CLIENTE_COLUNA_NOME + "TEXT," +
-                CLIENTE_COLUNA_EMAIL + " TEXT, " +
-                CLIENTE_COLUNA_CELULAR + " TEXT, " +
-                CLIENTE_COLUNA_CEP + " TEXT," +
-                CLIENTE_COLUNA_NUMERO + " TEXT)"
 
+        db.execSQL(
+                "CREATE TABLE " + ALUNO_TABELA_NOME +
+                        "(" + ALUNO_COLUNA_MATRICULA + " INTEGER  PRIMARY KEY , " +
+                        ALUNO_COLUNA_NOME + " TEXT, " +
+                        ALUNO_COLUNA_CPF + " TEXT, " +
+                        ALUNO_COLUNA_RELEVANCIA + " FLOAT)"
         );
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + CLIENTE_TABELA_NOME);
+        db.execSQL("DROP TABLE IF EXISTS " + ALUNO_TABELA_NOME);
         onCreate(db);
     }
 
-    public boolean insert (Cliente cli) {
-
-        SQLiteDatabase banco = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-
-        values.put(ClienteDados.CLIENTE_COLUNA_NOME, cli.nome);
-        values.put(ClienteDados.CLIENTE_COLUNA_EMAIL, cli.email);
-        values.put(ClienteDados.CLIENTE_COLUNA_CELULAR, cli.celular);
-        values.put(ClienteDados.CLIENTE_COLUNA_CEP, cli.cep);
-        values.put(ClienteDados.CLIENTE_COLUNA_NUMERO, cli.numero);
-
-        banco.insert(CLIENTE_TABELA_NOME, null, values);
-
-        return true;
-    }
-    public ArrayList<Cliente> ObterTodos() {
-        ArrayList<Cliente> retorno = new ArrayList<>();
+    
+    public ArrayList<Aluno> select() {
+        ArrayList<Aluno> retorno = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "SELECT * FROM " + CLIENTE_TABELA_NOME, null );
+        Cursor res =  db.rawQuery( "SELECT * FROM " + ALUNO_TABELA_NOME, null );
         if (res.moveToFirst()) {
             do {
-                Cliente cli = new Cliente();
+                Aluno a = new Aluno();
+                a.matricula = res.getInt(res.getColumnIndex(AlunoDados.ALUNO_COLUNA_MATRICULA));
+                a.nome = res.getString(res.getColumnIndex(AlunoDados.ALUNO_COLUNA_NOME));
+                a.cpf = res.getString(res.getColumnIndex(AlunoDados.ALUNO_COLUNA_CPF));
+                a.relevancia = res.getFloat(res.getColumnIndex(AlunoDados.ALUNO_COLUNA_RELEVANCIA));
 
-                cli.nome = res.getString(res.getColumnIndex(ClienteDados.CLIENTE_COLUNA_NOME));
-                cli.email = res.getString(res.getColumnIndex(ClienteDados.CLIENTE_COLUNA_EMAIL));
-                cli.celular = res.getString(res.getColumnIndex(ClienteDados.CLIENTE_COLUNA_CELULAR));
-                cli.cep = res.getString(res.getColumnIndex(ClienteDados.CLIENTE_COLUNA_CEP));
-                cli.numero = res.getString(res.getColumnIndex(ClienteDados.CLIENTE_COLUNA_NUMERO));
-
-                retorno.add(cli);
+                retorno.add(a);
             } while (res.moveToNext());
         }
         return retorno;
     }
+    public Aluno getAluno(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery(String.format("SELECT * FROM %s WHERE %s = ? ", ALUNO_TABELA_NOME, ALUNO_COLUNA_MATRICULA), new String[]{Integer.toString(id)});
+        Aluno retorno = new Aluno();
+        if (res.moveToFirst()) {
+            do {
+
+                retorno.matricula = res.getInt(res.getColumnIndex(AlunoDados.ALUNO_COLUNA_MATRICULA));
+                retorno.nome = res.getString(res.getColumnIndex(AlunoDados.ALUNO_COLUNA_NOME));
+                retorno.cpf = res.getString(res.getColumnIndex(AlunoDados.ALUNO_COLUNA_CPF));
+                retorno.relevancia = res.getFloat(res.getColumnIndex(AlunoDados.ALUNO_COLUNA_RELEVANCIA));
+
+
+            } while (res.moveToNext());
+        }
+        return retorno;
+    }
+    public boolean insert(Aluno a) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(ALUNO_COLUNA_NOME, a.nome);
+        contentValues.put(ALUNO_COLUNA_CPF, a.cpf);
+        contentValues.put(ALUNO_COLUNA_RELEVANCIA, a.relevancia);
+
+        db.insert(ALUNO_TABELA_NOME, null, contentValues);
+        return true;
+    }
+    public boolean update(Aluno a) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ALUNO_COLUNA_NOME, a.nome);
+        contentValues.put(ALUNO_COLUNA_CPF, a.cpf);
+        contentValues.put(ALUNO_COLUNA_RELEVANCIA, a.relevancia);
+        db.update(ALUNO_TABELA_NOME, contentValues, ALUNO_COLUNA_MATRICULA + " = ? ", new String[] { Integer.toString(a.matricula)} );
+        return true;
+    }
+    public Integer delete(Aluno a) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(ALUNO_TABELA_NOME,
+                ALUNO_COLUNA_MATRICULA + " = ? ",
+                new String[] { Integer.toString(a.matricula) });
+    }
+    public Integer deleteAllPerson() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(ALUNO_TABELA_NOME,
+                ALUNO_COLUNA_MATRICULA + " > ? ",
+                new String[] { Integer.toString(0) });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/gestormei/ClienteListAdapter01.java
@@ -0,0 +1,4 @@
+package com.example.gestormei;
+
+public class ClienteListAdapter01 {
+}
